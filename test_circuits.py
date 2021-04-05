b553197d2cd2b87019dd72887e60cb3aea1bffd7
goldsmdn <daniel.goldsmith.20@ucl.ac.uk>
2021-04-05 11:48:22 +0100

    #12 Improve testing framework

--- test_circuits.py
+++ test_circuits.py
@@ -1,16 +1,17 @@
 import pytest
+from pytest import raises
 from circuits import SteaneCodeLogicalQubit
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer
 
 TEST_X_QUBIT = 4
 SHOTS = 100     #Number of shots to run    
 SIMULATOR = Aer.get_backend('qasm_simulator')
+parity_check_matrix =  [[0,0,0,1,1,1,1],
+                        [0,1,1,0,0,1,1],
+                        [1,0,1,0,1,0,1]]
 
 def test_parity_validation():
-    """test that a random errors get fixed"""
-    parity_check_matrix =  [[0,0,0,1,1,1,1],
-                            [0,1,1,0,0,1,1],
-                            [1,0,1,0,1,0,1]]
+    """test that a random errors gets fixed"""
     for test_X_qubit in range(6):
         qubit = SteaneCodeLogicalQubit(1, parity_check_matrix, True)
         qubit.set_up_logical_zero(0)
@@ -21,8 +22,28 @@ def test_parity_validation():
         counts = result.get_counts(qubit)
         for key in counts.keys():
             assert(key[-7:]) == "0000000"
-        #length = len(result.get_counts(qubit))
-        #assert length == 1
+
+def test_no_error_correction_with_two_logical_qubits():
+    """check that an error is thrown if try error correction with two logical qubits"""
+    with raises(ValueError, match = "Can't correct errors with two logical qubits due to memory size restrictions"):
+        SteaneCodeLogicalQubit(2, parity_check_matrix, True)
+
+def test_logical_qubit_reference_in_range():
+    """check that an error is thrown if try and set up a logical zero with an index greater than 1"""
+    qubit = SteaneCodeLogicalQubit(2, parity_check_matrix, False)
+    with raises(ValueError, match = "The qubit to be processed must be indexed as 0 or 1 at present"):
+        qubit.set_up_logical_zero(2)
+
+def test_physical_qubit_reference_in_range():
+    """check that an error is thrown if try and index a physical qubit outside the valid range"""
+    qubit = SteaneCodeLogicalQubit(1, parity_check_matrix, False)
+    qubit.set_up_logical_zero(0)
+    with raises(ValueError, match = "Qubit index must be in range of data qubits"):
+        qubit.force_X_error(7,0)
+
+    
+
+
     
 
        
